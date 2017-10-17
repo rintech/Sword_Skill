4dc38855e3b4323b6528984ad27c121f9e7ae96a
tyuutorosan <kate3284@gmail.com>
2017-10-17 19:11:21 +0900

    テストコマンドのお試し実装

--- src/org/rintech/sword_skill/Main.java
+++ src/org/rintech/sword_skill/Main.java
@@ -1,16 +1,28 @@
 package org.rintech.sword_skill;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		System.out.println("プラグインが有効になりました");
+		System.out.println("プラグインが有効になりましたよー");
 	}
 
 	@Override
-	public void onDisable() {
-		System.out.println("プラグインが無効になりました");
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (label.equalsIgnoreCase("test")) {
+			sender.sendMessage("コマンドが実行されましたよー");
+		}
+	return true;
 	}
+	
+	@Override
+	public void onDisable() {
+		System.out.println("プラグインが無効になりましたよー");
+	}
+	
+	
 }
