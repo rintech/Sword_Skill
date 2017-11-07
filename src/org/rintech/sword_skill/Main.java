f35e5609cfa51166da1617ab3eceb7f9683c31c6
tyuutorosan <kate3284@gmail.com>
2017-11-07 19:35:08 +0900

    グラス（GLASS）とグラス（GRASS）がループするー

--- src/org/rintech/sword_skill/Main.java
+++ src/org/rintech/sword_skill/Main.java
@@ -1,7 +1,11 @@
 package org.rintech.sword_skill;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -18,11 +22,24 @@ public class Main extends JavaPlugin {
 		}
 	return true;
 	}
-	
+
+    @EventHandler
+  public void onPlayerJoin(BlockBreakEvent e) {
+	Block b = e.getBlock();
+	if (b.getType() == Material.GLASS) {
+		e.setCancelled(true);
+		b.setType(Material.GRASS);
+	}
+	if (b.getType() == Material.GRASS) {
+		e.setCancelled(true);
+		b.setType(Material.GLASS);
+	}
+}
+
 	@Override
 	public void onDisable() {
 		System.out.println("プラグインが無効になりましたよー");
 	}
-	
-	
+
+
 }
