7a4b7f859207de2d2c738b0652031141cf631ec8
tyuutorosan <kate3284@gmail.com>
2017-12-12 18:28:19 +0900

    よくわからない２

--- src/org/rintech/sword_skill/Main.java
+++ src/org/rintech/sword_skill/Main.java
@@ -7,16 +7,18 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements EventListner {
+public class Main extends JavaPlugin implements Listener {
 
 	@Override
 	public void onEnable() {
 		System.out.println("プラグインが有効になりましたよー");
 		System.out.println("はいったよー");
+		getServer().getPluginManager().registerEvents(this, this);
 	}
 
 	@Override
